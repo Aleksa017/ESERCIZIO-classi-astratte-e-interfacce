002b7258281ea0a2cba90f0c921b87d2e56f68fe
Aleksa <10934227@itisrossi.vi.it>
2024-10-15 13:04:31 +0200

    Aggiungere i file di progetto.

---
+++ ESERCIZIO 1 classi astratte e interfacce/CFigureGeometriche.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Formats.Asn1;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ESERCIZIO_1_classi_astratte_e_interfacce
+{
+    public abstract class CShape:IFiguraPiana 
+    {
+        public abstract double area();
+        public abstract double perimetro();
+
+
+
+    }
+    public interface IFiguraPiana
+    {
+        double area();
+        double perimetro();
+    }
+
+     public class CCircle : CShape
+    {
+        private float raggio;
+        private const double pi = 3.14;
+        public override double area()
+        {
+            double risultato = Math.Pow(raggio, 2);
+            return risultato;
+        }
+       public override double perimetro()
+        {
+            return 2*raggio*pi;
+        }   
+     }
+    public class CRectangle : CShape
+    {
+        private float b;
+        private float h;
+
+        public override double area()
+        {
+            return b *= h;
+        }
+        public override double perimetro()
+        {
+            return 2*(b+=h);
+        }
+    }
+
+    
+}
---
+++ ESERCIZIO 1 classi astratte e interfacce/IDrawable.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ESERCIZIO_1_classi_astratte_e_interfacce
+{
+    public interface IDrawable
+    {
+        void setColor();
+        void setPosition();
+        void draw();
+    }
+    public interface IEquatable<T>
+    {
+        bool Equals(T other);
+    }
+    public class DrawableCircle : CCircle, IDrawable, IEquatable<DrawableCircle>
+    {
+        public bool Equals(DrawableCircle other)
+        {
+            return true;
+        }
+        public void setColor()
+        {
+
+        }
+        public void setPosition()
+        {
+
+        }
+        public void draw()
+        {
+
+        }
+    }
+    public class DrawableRectangle : CCircle, IDrawable,IEquatable<DrawableRectangle>
+    {
+        public bool Equals(DrawableRectangle other)
+        {
+            return true;
+        }
+        public void setColor()
+        {
+
+        }
+        public void setPosition()
+        {
+
+        }
+        public void draw()
+        {
+
+        }
+    }
+}
